1cdf821b58e759f64eb05f71ef32de1a49ce35fc
Johan Quiroga <quirogacj@utp.edu.co>
2018-02-24 19:43:48 -0500

    Added support for images uploading

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const Dishes = require('./models/dishes');
 
@@ -60,6 +61,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
