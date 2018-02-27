ec63745094fad3dab5a535c1ca9940e2d1591274
Johan Quiroga <quirogacj@utp.edu.co>
2018-02-26 21:38:22 -0500

    Added Facebook OAuth2 authentication support

--- authenticate.js
+++ authenticate.js
@@ -1,11 +1,12 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken');
+const FacebookTokenStrategy = require('passport-facebook-token');
 
 const config = require('./config');
+const User = require('./models/user');
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 
@@ -46,3 +47,32 @@ exports.verifyAdmin = (req, res, next) => {
 
   next();
 };
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+  clientID: config.facebook.clientId,
+  clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+  User.findOne({facebookId: profile.id}, (err, user) => {
+    if (err) {
+      return done(err, false);
+    }
+
+    if (!err && user !== null) {
+      return done(null, user);
+    } else {
+      user = new User({username: profile.displayName});
+      user.facebookId = profile.id;
+      user.firstName = profile.name.givenName;
+      user.lastName = profile.name.familyName;
+
+      user.save((err, user) => {
+        if (err) {
+          return done(err, false);
+        } else {
+          return done(null, user);
+        }
+      });
+    }
+  });
+}));
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: '12345-67890-09876-54321',
-  mongoUrl: 'mongodb://localhost:27017/conFusion'
+  mongoUrl: 'mongodb://localhost:27017/conFusion',
+  facebook: {
+    clientId: '211954282883014',
+    clientSecret: 'eaaa006a2d77cd4d094f863b61138159'
+  }
 };
